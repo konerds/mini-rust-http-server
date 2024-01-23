716880611fc58a5754f2f83427cd62d75177f119
Konerd <adr10won@gmail.com>
2024-01-24 03:34:35 +0900

    feat: Implement handler

--- src/handler_web.rs
+++ src/handler_web.rs
@@ -1,11 +1,19 @@
-use super::http::{Request, Response, StatusHttp};
+use super::http::{Method, Request, Response, StatusHttp};
 use super::server::HandlerRequest;
 
 pub struct HandlerWeb;
 
 impl HandlerRequest for HandlerWeb {
     fn handle_success(&mut self, req: &Request) -> Response {
-        dbg!(req);
-        Response::new(StatusHttp::Ok, Some("<h1>Hello World!!!!</h1>".to_string()))
+        match req.method() {
+            Method::GET => match req.path() {
+                "/" => Response::new(StatusHttp::Ok, Some("<h1>Welcome</h1>".to_string())),
+                "/hello" => {
+                    Response::new(StatusHttp::Ok, Some("<h1>Hello World!</h1>".to_string()))
+                }
+                _ => Response::new(StatusHttp::NotFound, None),
+            },
+            _ => Response::new(StatusHttp::NotFound, None),
+        }
     }
 }
