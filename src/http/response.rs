75954bc0f5af51ff50d0c6c27375a3743da09e57
Konerd <adr10won@gmail.com>
2024-01-24 02:38:36 +0900

    feat: Implement response

--- src/http/response.rs
+++ src/http/response.rs
@@ -1,4 +1,7 @@
 use super::StatusHttp;
+use std::fmt::Debug;
+use std::io::{Result as ResultIo, Write};
+use std::net::TcpStream;
 
 #[derive(Debug)]
 pub struct Response {
@@ -9,5 +12,18 @@ pub struct Response {
 impl Response {
     pub fn new(status: StatusHttp, body: Option<String>) -> Self {
         Response { status, body }
+    }
+    pub fn send(&self, stream: &mut TcpStream) -> ResultIo<()> {
+        let body = match &self.body {
+            Some(b) => b,
+            None => "",
+        };
+        write!(
+            stream,
+            "HTTP/1.1 {} {}\r\n\r\n{}",
+            self.status,
+            self.status.get_cause(),
+            body
+        )
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,6 @@
-use crate::http::Request;
-use std::{io::Read, net::TcpListener};
+use crate::http::{Request, Response, StatusHttp};
+use std::io::{Read, Write};
+use std::net::TcpListener;
 
 pub struct Server {
     hostname: String,
@@ -16,19 +17,27 @@ impl Server {
         println!("Listening on {}:{}", self.hostname, self.port);
         loop {
             match listener.accept() {
-                Ok((mut s, _)) => {
+                Ok((mut stream, _)) => {
                     println!("The connection with has been established!");
                     let mut buffer = [0; 1024];
-                    match s.read(&mut buffer) {
+                    match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
-                            match Request::try_from(&buffer[..]) {
+                            let response = match Request::try_from(&buffer[..]) {
                                 Ok(req) => {
                                     dbg!(req);
+                                    Response::new(
+                                        StatusHttp::Ok,
+                                        Some("<h1>Hello World!!!!</h1>".to_string()),
+                                    )
                                 }
                                 Err(e) => {
-                                    println!("Failed to parse the contents of request: {}", e)
+                                    println!("Failed to parse the request: {}", e);
+                                    Response::new(StatusHttp::BadRequest, None)
                                 }
+                            };
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send response: {}", e);
                             }
                         }
                         Err(e) => println!("Failed to read: {}", e),
